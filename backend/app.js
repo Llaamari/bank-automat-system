afaa926496c9e9ead8b07e77b2fa4e421d7f7a86
Laura <laurasimila95@gmail.com>
2026-02-24 18:56:39 +0200

    Luotu kansiorakenne ja nginx.conf ja index.html tiedostot

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const healthRouter = require('./routes/health');
 const accountsRouter = require('./routes/accounts');
 const authRouter = require('./routes/auth');
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/health', healthRouter);
 app.use('/accounts', accountsRouter);
 app.use('/auth', authRouter);
